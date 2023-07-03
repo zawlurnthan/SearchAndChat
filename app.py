f59f222a70b9d8d4369f35c4b1dbfd502ef5d474
Zaw Than <zawlurnthan@gmail.com>
2023-07-02 21:57:09 -0400

    commit for searching part

--- app.py
+++ app.py
@@ -1,11 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from chat import answer_question
+from search import search_topic
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+# Main program
+def main():
+    topic = input('Enter a topic: ')
+
+    # Search the internet for information about the topic
+    title, link = search_topic(topic)
+
+    if title and link:
+        print(f"Top search result: {title}")
+        print(f"Link: {link}")
+
+        # Allow the user to ask questions about the topic
+        while True:
+            question = input('Ask a question (or type "exit" to quit): ')
+
+            if question.lower() == 'exit':
+                break
+
+            # Answer the user's question using the search result as context
+            answer = answer_question(question, title)
+
+            print(f"Answer: {answer}")
+    else:
+        print('Failed to retrieve search results')
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     user_input = ''
+#     processed_text = ''
+#     if request.method == 'POST':
+#         user_input = request.form['user_input']
+#         processed_text = user_input.upper()
+#     return render_template('index.html', user_input=user_input, processed_text=processed_text)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    topic = ''
+    data = {}
+    processed_text = ''
+    if request.method == 'POST':
+        topic = request.form['user_input']
+
+        # Search the internet for information about the topic
+        data = search_topic(topic)
+
+        processed_text = topic.upper()
+    return render_template('index.html', user_input=topic, data=data, processed_text=processed_text)
 
 
 if __name__ == '__main__':
---
+++ search.py
@@ -0,0 +1,38 @@
+# Function to search the internet using the Google Search API
+import os
+
+import requests
+
+
+def search_topic(topic):
+    # Set Google Search API key
+    api_key = os.environ['GOOGLE_API_KEY']
+
+    # Set the search engine ID
+    engine_id = os.environ['SEARCH_ENGINE_ID']
+
+    # Format the topic for search query
+    query = topic.replace(' ', '+')
+
+    # Set the Google Search API endpoint
+    url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={engine_id}&q={query}'
+
+    # Send a GET request to retrieve the search results
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Extract relevant information from the search results
+        # Extract the title and URL of each search result
+        # 1. get items list from the response json dictionary
+        # 2. get dictionary from the item lists
+        # 3. collect title and link from each dictionary
+
+        data = {}
+        for item in response.json()['items']:
+            data[item['title']] = item['link']
+
+        return data
+    else:
+        return None
+
+
