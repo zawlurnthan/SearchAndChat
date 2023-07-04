a49d3d1ae7dabf239719aeeb4d16eabd69215163
Zaw Than <zawlurnthan@gmail.com>
2023-07-03 22:33:59 -0400

    prevent AI on empty input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 from chat import answer_question
 from search import search_topic
@@ -36,11 +36,14 @@ def chat():
     if request.method == 'POST':
         # Allow the user to ask questions about the topic
         question = request.form['question']
-        # Answer the user's question using the search result as context
-        answer = answer_question(question)
 
-        # create a data structure to keep chatting
-        data.update({question: answer})
+        # prevent answer generating on empty input
+        if question != '':
+            # Answer the user's question using the search result as context
+            answer = answer_question(question)
+
+            # create a data structure to keep chatting
+            data.update({question: answer})
     return render_template('chat.html', question=question, data=data, answer=answer)
 
 
